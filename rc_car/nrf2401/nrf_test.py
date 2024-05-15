78e28d4096ffebade950dd04d9d401b164961551
MathiasAavaa <138600494+MathiasAavaa@users.noreply.github.com>
2024-05-15 16:11:20 -0400

    fv

--- rc_car/nrf2401/nrf_test.py
+++ rc_car/nrf2401/nrf_test.py
@@ -1,98 +1,62 @@
-import argparse
-from DateTime import DateTime
-import struct
-import sys
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Example program to receive packets from the radio link
+#
+
+import gpiod
+chip = gpiod.Chip('gpiochip4')
+line = chip.get_line(17)
+line.request(consumer="myApp", type=gpiod.LINE_REQ_DIR_OUT)
+
+
+from lib_nrf24 import NRF24
 import time
-import traceback
-from uuid import uuid4
+import spidev
 
-import pigpio
-from nrf24 import *
 
-#
-# A simple NRF24L receiver that connects to a PIGPIO instance on a hostname and port, default "localhost" and 8888, and
-# starts receiving data on the address specified sending a continiously increasing integer as acknowledgement payload.  
-# Use the companion program "ack-sender.py" to send data to it from a different Raspberry Pi.
-#
-if __name__ == "__main__":
 
-    print("Python NRF24 Receiver with Acknowledgement Payload Example.")
-    
-    # Parse command line argument.
-    parser = argparse.ArgumentParser(prog="ack-receiver.py", description="Simple NRF24 Receiver with Acknowledgement Payload.")
-    parser.add_argument('-n', '--hostname', type=str, default='localhost', help="Hostname for the Raspberry running the pigpio daemon.")
-    parser.add_argument('-p', '--port', type=int, default=8888, help="Port number of the pigpio daemon.")
-    parser.add_argument('address', type=str, nargs='?', default='1ACKS', help="Address to listen to (3 to 5 ASCII characters).")
+pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
 
-    args = parser.parse_args()
-    hostname = args.hostname
-    port = args.port
-    address = args.address
+radio2 = NRF24(line, spidev.SpiDev())
+radio2.begin(0, 17)
 
-    # Verify that address is between 3 and 5 characters.
-    if not (2 < len(address) < 6):
-        print(f'Invalid address {address}. Addresses must be between 3 and 5 ASCII characters.')
-        sys.exit(1)
-    
-    # Connect to pigpiod
-    # print(f'Connecting to GPIO daemon on {hostname}:{port} ...')
-    # pi = pigpio.pi(hostname, port)
-    # if not pi.connected:
-    #     print("Not connected to Raspberry Pi ... goodbye.")
-    #     exit()
+radio2.setRetries(15,15)
 
-    # Create NRF24 object.
-    # PLEASE NOTE: PA level is set to MIN, because test sender/receivers are often close to each other, and then MIN works better.
-    nrf = NRF24(pi, ce=25, payload_size=RF24_PAYLOAD.ACK, channel=100, data_rate=RF24_DATA_RATE.RATE_250KBPS, pa_level=RF24_PA.MIN)
-    nrf.set_address_bytes(len(address))
+radio2.setPayloadSize(32)
+radio2.setChannel(0x60)
+radio2.setDataRate(NRF24.BR_2MBPS)
+radio2.setPALevel(NRF24.PA_MIN)
 
-    # Listen on the address specified as parameter
-    nrf.open_reading_pipe(RF24_RX_ADDR.P1, address)
-    
-    # Display the content of NRF24L01 device registers.
-    nrf.show_registers()
+radio2.setAutoAck(True)
+radio2.enableDynamicPayloads()
+radio2.enableAckPayload()
 
-    # Set the UUID that will be the payload of the next acknowledgement.
-    next_id = 1
-    nrf.ack_payload(RF24_RX_ADDR.P1, struct.pack('<I', next_id))
-    
-    try:
-        # Enter a loop receiving data on the address specified.
-        print(f'Receive data on {address}')
-        count = 0
-        while True:
+radio2.openWritingPipe(pipes[0])
+radio2.openReadingPipe(1, pipes[1])
 
-            # As long as data is ready for processing, process it.
-            while nrf.data_ready():
-                # Count message and record time of reception.            
-                count += 1
-                now = datetime.now()
-                
-                # Read pipe and payload for message.
-                pipe = nrf.data_pipe()
-                payload = nrf.get_payload()    
+radio2.startListening()
+radio2.stopListening()
 
-                # Hex the payload received.
-                hex = ':'.join(f'{i:02x}' for i in payload)
+radio2.printDetails()
 
-                # Show message received as hex.
-                print(f"{now:%Y-%m-%d %H:%M:%S.%f}: pipe: {pipe}, len: {len(payload)}, bytes: {hex}, count: {count}, next_id={next_id}")
+radio2.startListening()
 
-                # If the length of the message is 9 bytes and the first byte is 0x01, then we try to interpret the bytes
-                # sent as an example message holding a temperature and humidity.
-                if len(payload) == 9 and payload[0] == 0x01:
-                    
-                    values = struct.unpack("<Bff", payload)
-                    print(f'Protocol: {values[0]}, temperature: {values[1]}, humidity: {values[2]}')
-                    
-                    # Set uuid that will be part of the next acknowledgement.
-                    next_id += 1
-                    nrf.ack_payload(RF24_RX_ADDR.P1, struct.pack('<I', next_id))
-                
-            # Sleep 1 ms.
-            time.sleep(0.001)
+c=1
+while True:
+    akpl_buf = [c,1, 2, 3,4,5,6,7,8,9,0,1, 2, 3,4,5,6,7,8]
+    pipe = [0]
+    while not radio2.available(pipe):
+        time.sleep(10000/1000000.0)
 
-    except:
-        traceback.print_exc()
-        nrf.power_down()
-        pi.stop()
+    recv_buffer = []
+    radio2.read(recv_buffer, radio2.getDynamicPayloadSize())
+    print ("Received:") ,
+    print (recv_buffer)
+    c = c + 1
+    if (c&1) == 0:
+        radio2.writeAckPayload(1, akpl_buf, len(akpl_buf))
+        print ("Loaded payload reply:"),
+        print (akpl_buf)
+    else:
+        print ("(No return payload)")
