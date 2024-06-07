06cffc86b8c998edd8ca513128f76ef7e159ca4e
Mathias <eltopchi3@gmail.com>
2024-06-06 22:57:27 -0400

    juv

--- arm/MIL_tracker.py
+++ arm/MIL_tracker.py
@@ -7,7 +7,7 @@ import numpy as np
 from turret import Turret
 
 def main():
-    local_model_path = "../models--arnabdhar--YOLOv8-Face-Detection/snapshots/52fa54977207fa4f021de949b515fb19dcab4488/model.pt"
+    local_model_path = "models--arnabdhar--YOLOv8-Face-Detection/snapshots/52fa54977207fa4f021de949b515fb19dcab4488/model.pt"
     
     if not os.path.exists(local_model_path):
         print(f"../Model file not found at {local_model_path}. Please download it first.")
@@ -81,8 +81,13 @@ def main():
                 screen_center_y = frame.shape[0] // 2
                 face_diff_x = face_center_x - screen_center_x
                 face_diff_y = face_center_y - screen_center_y
+                print(f"Tracking face center: x={face_diff_x}, y={face_diff_y}")
                 
-                turret.update(face_diff_x, face_diff_y)
+                if abs(face_diff_x) > 10 or abs(face_diff_y) > 10:
+                    # Update turret position
+                    turret.update(face_diff_x, face_diff_y)
+                
+                
             else:
                 tracking = False
 
