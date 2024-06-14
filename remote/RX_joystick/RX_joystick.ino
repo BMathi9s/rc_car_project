6a9abad69b0fb3636aa4247a28e5f0e12bf889a7
Ricardo Raji Chahine <ricardochahine0@gmail.com>
2024-06-14 16:28:58 -0400

    Integrated servo code

--- remote/RX_joystick/RX_joystick.ino
+++ remote/RX_joystick/RX_joystick.ino
@@ -2,11 +2,16 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Arduino.h>
+#include <ESP32Servo.h> 
+//esp32servo by kevin 
+// only work by using version 1.2.1!!
+#define BASESERVO_PIN 26      // GPIO pin used to connect the servo control (digital out)
+#define HEADSERVO_PIN 33
 
 RF24 radio(4, 5); // (CE, CSN)
 
 byte engineJoystickData[2]; // Array to hold X and Y values for the engines
-byte servoJoystickData[2]; //Array to hold X and Y values for servo
+byte servoJoystickData[2]; // Array to hold X and Y values for servo
 
 #define MOTOR_DIR_L 13
 #define MOTOR_PWM_L 12
@@ -27,6 +32,14 @@ const int resolution = 8;     // 8-bit resolution (0-255)
 const byte deadZoneMin = 98; // 118 - 20
 const byte deadZoneMax = 138; // 118 + 20
 
+// servo motors
+Servo head; // (x)
+Servo base; // (y)
+
+// initialize 
+int xShift = 90;
+int yShift = 90;
+
 void setup() {
     Serial.begin(9600); // Begin Serial communication
 
@@ -46,21 +59,42 @@ void setup() {
     radio.startListening(); // Set receiver mode
 
     delay(10);
+
+    radio.setPayloadSize(4);  // this sets the package size to 4 bytes (default, max: 32 bytes) - 4 bytes for joystick values. might increase range
+    radio.enableAckPayload(); // enable automatic acknowledge signals
+    radio.setAutoAck(1);
+    radio.setRetries(5, 5); // (delay, max no.of retries)
+    radio.setAutoAck(true);
+
+    radio.openReadingPipe(1, 0x1234567890LL); // set address at which nrf24 will communicate
+    radio.startListening(); // set receiver mode
+
+    servo_init();
 }
 
 void loop() {
     if (radio.available()) {
         radio.read(&engineJoystickData, sizeof(engineJoystickData));
-        byte x = engineJoystickData[0];
-        byte y = engineJoystickData[1];
+        radio.read(&servoJoystickData, sizeof(servoJoystickData));
 
-        Serial.print("Joystick X: ");
-        Serial.print(x);
-        Serial.print(" | Joystick Y: ");
-        Serial.println(y);
+        byte x1 = engineJoystickData[0];
+        byte y1 = engineJoystickData[1];
 
-        handleJoystickInput(x, y);
+        byte x2 = servoJoystickData[0]; 
+        byte y2 = servoJoystickData[1]; 
+
+        Serial.print("Joystick1 X: ");
+        Serial.print(x1);
+        Serial.print(" | Joystick1 Y: ");
+        Serial.println(y1);
+
+        handleJoystickInput(x1, y1);
+        handleServoJoystick(x2, y2);
+
+        lastReceiveTime = millis();
     }
+
+    checkTimeout(); // Check for timeout condition
 
     delay(10); // Adding a small delay for smoother control
 }
@@ -95,15 +129,11 @@ void handleJoystickInput(byte x, byte y) {
 
 // Function to calculate motor speeds based on joystick inputs
 void calculateMotorSpeeds(float mappedX, float mappedY, float &speedL, float &speedR) {
-    
+    // Diagonal Movement
+    float turnFactor = ((abs(mappedY) / (float)MAX_SPEED)) * (abs(mappedX) / (float)MAX_SPEED);
 
-    //Diagonal Movement
-   
-      float turnFactor = ((abs(mappedY) / (float)MAX_SPEED)) * (abs(mappedX) / (float)MAX_SPEED);
-
-      speedL = mappedY + (turnFactor * mappedX);
-      speedR = mappedY - (turnFactor * mappedX );
-    
+    speedL = mappedY + (turnFactor * mappedX);
+    speedR = mappedY - (turnFactor * mappedX);
 }
 
 float exponentialDecay(float input, float decayRate) {
@@ -121,6 +151,47 @@ void setMotorSpeed(int speed, int dirPin, int pwmPin) {
     analogWrite(pwmPin, speed);
 }
 
+void servo_init(){
+    // Allow allocation of all timers
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
+    base.setPeriodHertz(50); // Standard 50hz servo
+    base.attach(BASESERVO_PIN, 500, 2400);
+    head.setPeriodHertz(50);
+    head.attach(HEADSERVO_PIN, 500, 2400); 
+}
+
+void handleServoJoystick(byte x, byte y) {
+    const byte deadZoneMin = 128 - 20;
+    const byte deadZoneMax = 128 + 20;
+
+    if (x >= deadZoneMin && x <= deadZoneMax && y >= deadZoneMin && y <= deadZoneMax) {
+        // do nothing
+    } else {
+        if (x > deadZoneMax) {
+            if (xShift <= 180)
+                xShift++;
+        }
+        if (x < deadZoneMin) {
+            if (xShift >= 0)
+                xShift--;
+        }
+        if (y > deadZoneMax) {
+            if (yShift <= 180)
+                yShift++;
+        }
+        if (y < deadZoneMin) {
+            if (yShift >= 0)
+                yShift--;
+        }
+    }
+
+    head.write(xShift);
+    base.write(yShift);
+}
+
 // Stop the motors
 void stop() {
     Serial.println("Stop");
@@ -134,7 +205,6 @@ void checkTimeout() {
         // Timeout action here
         Serial.println("Transmission timeout");
         lastReceiveTime = currentMillis; // Reset the timer
-        // stop();
         resetReception(); // Reset reception
     }
 }
