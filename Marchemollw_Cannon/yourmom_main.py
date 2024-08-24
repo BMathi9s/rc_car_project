4e6ce8af9f84214f15fc74adfcc19117836da371
sbiothmane <114199239+sbiothmane@users.noreply.github.com>
2024-08-24 18:46:33 -0400

    Create yourmom_main.py

---
+++ Marchemollw_Cannon/yourmom_main.py
@@ -0,0 +1,79 @@
+import cv2
+import mediapipe as mp
+import time
+
+from Marshmellow_Cannon import Marshmellow_Cannon
+# Example usage
+
+
+# Initialize MediaPipe Pose
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+mp_drawing = mp.solutions.drawing_utils
+
+# Initialize video capture with lower resolution
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+# Set frame rate limit
+fps_limit = 90
+prev_time = 0
+center_of_screen = 0.5
+
+#ini cannon
+cannon = Marshmellow_Cannon(base_channel=0, cannon_channel=1)
+cannon.center()
+cannon.set_camera_scope(16)
+
+while cap.isOpened():
+    time_elapsed = time.time() - prev_time
+    if time_elapsed > 1./fps_limit:
+        prev_time = time.time()
+
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convert the frame to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Process the frame and find pose
+        results = pose.process(image)
+
+        # Convert back to BGR for OpenCV
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Draw the pose annotation on the image and get the nose coordinates
+        if results.pose_landmarks:
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            
+            # Get the nose coordinates 
+            nose = results.pose_landmarks.landmark[0]
+            size1 = results.pose_landmarks.landmark[2]
+            size2 = results.pose_landmarks.landmark[5]
+            deltax = size2.x - size1.x
+            
+            print(f'Nose coordinates: {nose}')
+      
+            
+            print(f'dif x: {deltax}')
+            lowerlimit, highlimit = 0.49, 0.51
+            
+            if lowerlimit <= nose.x <=  highlimit and lowerlimit <= nose.y <= highlimit:
+                pass  # Do nothing to avoid overshoot
+            elif lowerlimit <= nose.x <= highlimit and not lowerlimit <= nose.y <= highlimit:
+                cannon.track_face(nose.x, 0.5,deltax)
+            elif (not lowerlimit <= nose.x <= highlimit) and lowerlimit <= nose.y <= highlimit:
+                cannon.track_face(0.5,nose.y,deltax)
+            else:
+                cannon.track_face(nose.x, nose.y, deltax)  # Adjust servos to track the face
+            # Check conditions and call cannon.track_face accordingly
+            
+            
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
