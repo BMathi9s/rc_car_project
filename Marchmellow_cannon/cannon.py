4b92e9260ef6a63594c06b2f586ab95beb87e6c4
sbiothmane <114199239+sbiothmane@users.noreply.github.com>
2024-08-24 19:16:19 -0400

    Update cannon.py

--- Marchmellow_cannon/cannon.py
+++ Marchmellow_cannon/cannon.py
@@ -22,7 +22,7 @@ center_of_screen = 0.5
 # Initialize the cannon with specific function types for x and y axis movements
 cannon = turret(base_channel=0, cannon_channel=1, function_type_x='sigmoid', function_type_y='cubic')
 cannon.center()
-cannon.set_camera_scope(25)
+cannon.set_camera_scope(18)
 
 while cap.isOpened():
     time_elapsed = time.time() - prev_time
